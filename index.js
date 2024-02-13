267bec61398f01fb2b272cdf6440e22163db7749
Tarun <tarunmalviya8827@gmail.com>
2024-02-13 11:29:50 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config()
-const port =process.env.PORT||8000;
+const port =process.env.BASE_URL||8000;
 const app = express();
 const path= require('path');
 const mongoose= require('./config/mongoose');
